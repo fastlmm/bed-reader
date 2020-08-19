df3e81f57ee91138b0d6cd560b296eae6b603e44
Carl Kadie <carlk@msn.com>
2020-08-19 11:03:10 -0700

    travis

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
         "Programming Language :: Python",
     ],
     packages=["bed_reader","bed_reader/tests"],  # basically everything with a __init__.py
-    package_data={"bed_reader":["external/intel/windows/compiler/lib/intel64/libiomp5md.dll"]} if "win" in platform.system().lower() else {},
+    data_files=[("bed_reader", ["external/intel/windows/compiler/lib/intel64/libiomp5md.dll"])] if "win" in platform.system().lower() else [],
     install_requires=install_requires,
     # extensions
     cmdclass=cmdclass,
