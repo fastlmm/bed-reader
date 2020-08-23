e6cc9ad745786ace13bf2bf22310ce9dc60d199c
Carl Kadie <carlk@msn.com>
2020-08-23 12:57:52 -0700

    travis

--- bed_reader/_open_bed.py
+++ bed_reader/_open_bed.py
@@ -517,7 +517,7 @@ class open_bed:  #!!!cmk need doc strings everywhere
                 found_ver = open_bed._get_version_number(find_location)
                 goal_ver = (5, 0, 2014, 926)
                 print(f"cmk found ver is '{found_ver}'. Goal ver is '{goal_ ver}'")
-                if  found_ver >= goal_ ver:
+                if  found_ver >= goal_ver:
                     print(f"cmk found version looks good, so wait and load that)
                     return
             location_list = [
