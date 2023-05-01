3894408882545a9e7b9efe3f6714bbdd116bc3cd
Carl Kadie <carlk@msn.com>
2023-05-01 14:43:10 -0700

    try to fix flake8

--- bed_reader/__init__.py
+++ bed_reader/__init__.py
@@ -1,17 +1,17 @@
 """Read and write the PLINK BED format, simply and efficiently."""
 
+# flake8: noqa: F401
+
 # See: https://towardsdatascience.com/nine-rules-for-writing-python-extensions-in-rust-d35ea3a4ec29?sk=f8d808d5f414154fdb811e4137011437
 # for an article on how this project uses Rust to create a Python extension.
 
-from bed_reader._open_bed import get_num_threads, open_bed  # noqa
-from bed_reader._sample_data import sample_file, tmp_path  # noqa
-from bed_reader._to_bed import to_bed  # noqa
+from bed_reader._open_bed import get_num_threads, open_bed
+from bed_reader._sample_data import sample_file, tmp_path
+from bed_reader._to_bed import to_bed
 
-# type: ignore
-from .bed_reader import file_aat_piece_f32_orderf  # noqa
-from .bed_reader import (file_aat_piece_f64_orderf, file_ata_piece_f32_orderf,
-                         file_ata_piece_f64_orderf, file_b_less_aatbx,
-                         file_dot_piece, read_f32, read_f64, read_i8,
-                         standardize_f32, standardize_f64, subset_f32_f32,
-                         subset_f32_f64, subset_f64_f64, write_f32, write_f64,
-                         write_i8)
+from .bed_reader import (file_aat_piece_f32_orderf, file_aat_piece_f64_orderf,
+                         file_ata_piece_f32_orderf, file_ata_piece_f64_orderf,
+                         file_b_less_aatbx, file_dot_piece, read_f32, read_f64,
+                         read_i8, standardize_f32, standardize_f64,
+                         subset_f32_f32, subset_f32_f64, subset_f64_f64,
+                         write_f32, write_f64, write_i8)
