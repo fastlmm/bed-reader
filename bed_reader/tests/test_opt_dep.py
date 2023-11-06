fbfa82d955aeed11b061c3d45c105dcf529f4ef3
Carl Kadie <carlk@msn.com>
2023-11-05 17:20:07 -0800

    again5

--- bed_reader/tests/test_opt_dep.py
+++ bed_reader/tests/test_opt_dep.py
@@ -12,7 +12,7 @@ def test_optional_dependencies(shared_datadir):
     file = shared_datadir / "plink_sim_10s_100v_10pmiss.bed"
     with open_bed(file) as bed:
         if sprase is None:
-            with pytest.raises(ValueError):
+            with pytest.raises(ImportError):
                 _ = bed.read_sparse(dtype="int8")
         else:
             _ = bed.read_sparse(dtype="int8")
@@ -23,7 +23,7 @@ def test_optional_dependencies(shared_datadir):
         pooch = None
 
     if pooch is None:
-        with pytest.raises(ValueError):
+        with pytest.raises(ImportError):
             _ = sample_file("small.bed")
     else:
         _ = sample_file("small.bed")
